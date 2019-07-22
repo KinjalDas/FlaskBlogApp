209a86776baf9db34d17fd5dbc3b4de8bd8781c7
Kinjal Das <kinjal.das86@gmail.com>
2019-07-22 23:12:55 +0530

    progress on User Accounts

--- app.py
+++ app.py
@@ -3,20 +3,38 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client.MyBlogDB
+users = db["users"]
+posts = db["posts"]
 
 app = Flask(__name__)
+
+def generateErrTemplate(err):
+    return render_template('error.html',err=err)
+
+def UserName_or_Email_Exists(username,email):
+    if ((username != None) and (users.find({"UserName" : username}).count()) == 0):
+        return False
+    elif ((email != None) and (users.find({"Email" : email}).count()) == 0):
+        return False
+    else:
+        return True
 
 @app.route('/',methods=['GET','POST'])
 def hello_world():
     if request.method == "GET":
         return render_template('index.html',db=db)
+    if request.method == "POST":
+        type = request.form["type"]
+        if type == "register":
+            email = request.form["RegisterEmail"]
+            username = request.form["RegisterUserName"]
+            password = request.form["RegisterPassword"]
+            if (UserName_or_Email_Exists(username,email):
+                generateErrTemplate("UserName/Email Already Exists!")
 
-@app.route('/register_or_login',methods=['GET','POST'])
-def register_or_login():
-    if request.method == "GET":
-        return render_template('register_login.html',register_or_login_page=True,db=db)
+        return render_template('index.html',register_or_login_page=True,db=db,type=type)
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
